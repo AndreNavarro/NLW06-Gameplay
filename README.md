0c65617783e90f678a5c3ec6db09d688323095f0
AndreNavarro <andre-navarro@hotmail.com>
2022-04-21 16:53:12 -0300

    :memo: Update Readme - #NLW06

---
+++ README.md
@@ -0,0 +1,43 @@
+<h1 align="center">
+  Gameplay
+</h1>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-como-executar">Como executar</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-licença">Licença</a>
+</p>
+
+<br>
+
+---
+
+<p align="center">
+🚧 PROJETO EM ANDAMENTO 🚧
+</p>
+
+---
+
+<br>
+
+## ✨ Tecnologias
+
+[ ... ]
+
+## 💻 Projeto
+
+[ ... ]
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [desse link](https://www.figma.com/file/5hcGw1nq8ccuyJofpsEnWN/GamePlay---NLW-Together-(Copy)?node-id=58913%3A83). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+## 🚀 Como Executar
+
+[ ... ]
+
+## 📝 Licença
+
+Projeto desenvolvido pela Rocketseat e apresentado na **Next Level Week - Together** (nlw #06).
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,69 +1,67 @@
-import React, 
-  { 
-    createContext,
-    ReactNode,
-    useContext,
-    useState ,
-    useEffect
-  } from 'react';
+import React,
+{
+  createContext,
+  ReactNode,
+  useContext,
+  useState,
+  useEffect
+} from 'react';
 
-  import * as AuthSession from 'expo-auth-session';
-  import AsyncStorage from '@react-native-async-storage/async-storage';
-  import { api } from '../services/api';
-  import { COLLECTION_USER } from '../configs/database'
-  
-  
-  const { SCOPE } = process.env;
-  const { CLIENT_ID } = process.env;
-  const { CDN_IMAGE } = process.env;
-  const { REDIRECT_URI } = process.env;
-  const { RESPONSE_TYPE } = process.env;
+import * as AuthSession from 'expo-auth-session';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { api } from '../services/api';
+import { COLLECTION_USER } from '../configs/database'
 
-
+/*
+const { SCOPE } = process.env;
+const { CLIENT_ID } = process.env;
+const { CDN_IMAGE } = process.env;
+const { REDIRECT_URI } = process.env;
+const { RESPONSE_TYPE } = process.env;
 
 
-  type User = {
-    id: string;
-    username: string;
-    firstName: string;
-    avatar: string;
-    email: string;
-    token: string;
+type User = {
+  id: string;
+  username: string;
+  firstName: string;
+  avatar: string;
+  email: string;
+  token: string;
+}
+
+type AuthContextData = {
+  user: User;
+  loading: boolean;
+  signIn: () => Promise<void>;
+}
+
+type AuthProviderProps = {
+  children: ReactNode;
+}
+
+type AuthorizationResponse = AuthSession.AuthSessionResult & {
+  params: {
+    access_token: string;
+    error?: string;
   }
-
-  type AuthContextData = {
-    user: User;
-    loading: boolean;
-    signIn: () => Promise<void>;
-  }
-
-  type AuthProviderProps = {
-    children: ReactNode;
-  }
-
-  type AuthorizationResponse = AuthSession.AuthSessionResult & {
-    params: {
-      access_token: string;
-      error?: string;
-    }
-  }
+}
 
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>({} as User);
-  const [loading, setLoading]= useState(false);
+  const [loading, setLoading] = useState(false);
 
-  async function signIn(){
+  async function signIn() {
     try {
       setLoading(true);
 
       const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
       const { type, params } = await AuthSession
-      .startAsync({ authUrl }) as AuthorizationResponse;
+        .startAsync({ authUrl }) as AuthorizationResponse;
 
-      if(type === "success" && !params.error){
+      if (type === "success" && !params.error) {
         api.defaults.headers.authorization = `Bearer ${params.access_token}`;
 
         const userInfo = await api.get('/users/@me');
@@ -91,7 +89,7 @@ function AuthProvider({ children }: AuthProviderProps) {
   async function loadUserStorageData() {
     const storage = await AsyncStorage.getItem(COLLECTION_USER);
 
-    if(storage){
+    if (storage) {
       const userLogged = JSON.parse(storage) as User;
       api.defaults.headers.authorization = `Bearer ${userLogged.token}`;
 
@@ -103,13 +101,13 @@ function AuthProvider({ children }: AuthProviderProps) {
     loadUserStorageData();
   }, [])
 
-  return(
+  return (
     <AuthContext.Provider value={{
       user,
       loading,
       signIn
     }}>
-      { children }
+      {children}
     </AuthContext.Provider>
   )
 }
